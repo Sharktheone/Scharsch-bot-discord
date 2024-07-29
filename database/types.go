925b27368a1ca484d3a2a4afc28c5499197c0150
Shark <sharktheone@proton.me>
2024-07-29 16:30:10 +0200

    add tags for gorm

--- database/types.go
+++ database/types.go
@@ -1,35 +1,35 @@
 package database
 
 type WhitelistEntry struct {
-	UserID UserID `json:"userID" bson:"userID"`
-	Player Player `json:"player" bson:"player"`
+	UserID UserID `json:"userID" bson:"userID" gorm:"userID"`
+	Player Player `json:"player" bson:"player" gorm:"player"`
 }
 
 type BanEntry struct {
-	UserID     UserID          `json:"userID" bson:"userID"`
-	UserBan    bool            `json:"userBan" bson:"userBan"`
-	Players    []PlayerBanData `json:"players" bson:"players"`
-	UserReason string          `json:"reason" bson:"reason"`
+	UserID     UserID          `json:"userID" bson:"userID" gorm:"userID"`
+	UserBan    bool            `json:"userBan" bson:"userBan" gorm:"userBan"`
+	Players    []PlayerBanData `json:"players" bson:"players" gorm:"players"`
+	UserReason string          `json:"reason" bson:"reason" gorm:"userReason"`
 }
 
 type PlayerBanData struct {
-	Player Player `json:"player" bson:"player"`
-	Reason string `json:"reason" bson:"reason"`
+	Player Player `json:"player" bson:"player" gorm:"player"`
+	Reason string `json:"reason" bson:"reason" gorm:"reason"`
 }
 
-type ReWhitelsitEntry struct {
-	UserID      UserID   `json:"userID" bson:"userID"`
-	Players     []Player `json:"players" bson:"players"`
-	MissingRole Role     `json:"missingRole" bson:"missingRole"`
+type ReWhitelistEntry struct {
+	UserID      UserID   `json:"userID" bson:"userID" gorm:"userID"`
+	Players     []Player `json:"players" bson:"players" gorm:"players"`
+	MissingRole Role     `json:"missingRole" bson:"missingRole" gorm:"missingRole"`
 }
 
 type ReportEntry struct {
-	ReporterID     UserID `json:"reporterID" bson:"reporterID"`
-	ReportedPlayer Player `json:"reportedPlayer" bson:"reportedPlayer"`
+	ReporterID     UserID `json:"reporterID" bson:"reporterID" gorm:"reporterID"`
+	ReportedPlayer Player `json:"reportedPlayer" bson:"reportedPlayer" gorm:"reportedPlayer"`
 	Reason         string `json:"reason" bson:"reason"`
 }
 
 type WaitlistEntry struct {
-	UserID UserID `json:"userID" bson:"userID"`
-	Player Player `json:"player" bson:"player"`
+	UserID UserID `json:"userID" bson:"userID" gorm:"userID"`
+	Player Player `json:"player" bson:"player" gorm:"player"`
 }
