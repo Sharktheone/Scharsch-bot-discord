9a4267c29abcb804f40456d05f8fb0f1baa77577
Shark <sharktheone@proton.me>
2024-12-22 21:44:21 +0100

    fix console timer

--- pterodactyl/listeners/consoleTimer.go
+++ pterodactyl/listeners/consoleTimer.go
@@ -3,7 +3,6 @@ package listeners
 import "time"
 
 type consoleTimer struct {
-	enabled bool
 	t       *time.Timer
 	started bool
 	maxTime time.Duration
@@ -12,9 +11,7 @@ type consoleTimer struct {
 
 func newTimer(maxTime string) (*consoleTimer, error) {
 	if maxTime == "" {
-		return &consoleTimer{
-			enabled: false,
-		}, nil
+		return &consoleTimer{}, nil
 	}
 	m, err := time.ParseDuration(maxTime)
 	if err != nil {
@@ -26,9 +23,6 @@ func newTimer(maxTime string) (*consoleTimer, error) {
 }
 
 func (t *consoleTimer) start() {
-	if !t.enabled {
-		return
-	}
 	if t.started {
 		return
 	}
@@ -37,15 +31,9 @@ func (t *consoleTimer) start() {
 	t.started = true
 }
 func (t *consoleTimer) reset() {
-	if !t.enabled {
-		return
-	}
 	t.t.Reset(t.maxTime)
 }
 func (t *consoleTimer) stop() {
-	if !t.enabled {
-		return
-	}
 	t.t.Stop()
 	t.started = false
 }
